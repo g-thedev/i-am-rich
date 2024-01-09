9104707c337a5e28c2f7156965b3d4069394d472
Gerard Fajardo <gerard@zeta.haus>
2024-01-08 20:24:16 -0800

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Gerard Fajardo on 2024-01-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
